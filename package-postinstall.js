4ac4b263043216efcb1cee945cbe6db8a1613443
Rosano Coutinho <pub@xeari.com>
2020-11-29 12:25:06 -0300

    Fix typo

--- package-postinstall.js
+++ package-postinstall.js
@@ -15,6 +15,7 @@
 	require('./node_modules/OLSKExpress/modules/OLSKAssets/main.js').OLSKAssetsCopyAssetsFromTo([
 		'normalize.css',
 		'OLSKLayout',
+		'OLSKRootLink',
 		'ROCORootLink',
 	], require('path').join(__dirname, 'node_modules'), require('path').join(__dirname, 'os-app/_shared/__external'));
 })();
