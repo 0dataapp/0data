01cf3c995e64bb380a5dc02bf2f39fc5e18ff036
Rosano Coutinho <pub@xeari.com>
2020-11-29 10:36:45 -0300

    Spec fetch and cache urls

--- os-app/open-vitrine/controller-tests.js
+++ os-app/open-vitrine/controller-tests.js
@@ -9,3 +9,90 @@ describe('DataFetchURLs', function test_DataFetchURLs() {
 	});
 
 });
+
+describe('LifecycleModuleDidLoad', function test_LifecycleModuleDidLoad() {
+
+	const _LifecycleModuleDidLoad = function (inputData) {
+		return Object.assign(Object.assign({}, mod), {
+			_DataContentString: (function () {}),
+
+			_DataFoilOLSKCache: Object.assign({
+				OLSKCacheResultFetchRenew: (function (inputData) {
+					if (inputData.ParamCallback) {
+						inputData.ParamCallback();
+					}
+
+					if (inputData._ParamCallbackDidFinish) {
+						inputData._ParamCallbackDidFinish();
+					}
+				}),
+				OLSKCacheWriteFile: (function () {}),
+			}, inputData),
+		}, inputData).LifecycleModuleDidLoad();
+	};
+
+	it('calls OLSKCacheResultFetchRenew', function () {
+		const item = [];
+
+		const ParamMap = {
+			alfa: Math.random().toString(),
+		};
+
+		_LifecycleModuleDidLoad({
+			_ValueCache: ParamMap,
+			OLSKCacheResultFetchRenew: (function () {
+				item.push(...arguments);
+			}),
+		});
+
+		deepEqual(item, mod.DataFetchURLs().map(function (ParamKey, i) {
+			return {
+				ParamMap,
+				ParamKey,
+				ParamCallback: item[i].ParamCallback,
+				ParamInterval: 1000 * 60 * 60 * 24,
+				_ParamCallbackDidFinish: item[i]._ParamCallbackDidFinish,
+			}
+		}));
+	});
+
+	context('ParamCallback', function () {
+
+		it('calls _DataContentString', function () {
+			const item = [];
+
+			_LifecycleModuleDidLoad({
+				_DataContentString: (function () {
+					item.push(...arguments);
+				}),
+			});
+
+			deepEqual(item, mod.DataFetchURLs());
+		});
+	
+	});
+
+	context('_ParamCallbackDidFinish', function () {
+
+		it('calls _DataContentString', function () {
+			const item = [];
+
+			const _ValueCache = {
+				alfa: Math.random().toString(),
+			};
+
+			_LifecycleModuleDidLoad({
+				_ValueCache,
+				OLSKCacheWriteFile: (function () {
+					item.push(Array.from(arguments));
+				}),
+			});
+
+			deepEqual(item, mod.DataFetchURLs().map(function () {
+				return [_ValueCache, require('path').basename(__dirname), require('path').join(__dirname, '__cached')];
+			}));
+		});
+	
+	});
+
+});
--- os-app/open-vitrine/controller.js
+++ os-app/open-vitrine/controller.js
@@ -1,3 +1,24 @@
+// How to get node.js HTTP request promise without a single dependency https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/
+const uGet = function (inputData) {
+  return new Promise((resolve, reject) => {
+    (inputData.startsWith('https') ? require('https') : require('http')).get(inputData, (response) => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+      	return reject(new Error('Error: ' + response.statusCode));
+      }
+
+      const body = [];
+
+      response.on('data', function (chunk) {
+      	return body.push(chunk);
+      });
+
+      response.on('end', function () {
+      	return resolve(body.join(''))
+      });
+    });
+  });
+};
+
 const mod = {
 
 	OLSKControllerRoutes () {
@@ -18,12 +39,43 @@ const mod = {
 		return process.env.NODE_ENV === 'development';
 	},
 
+	// VALUE
+
+	_ValueCache: require('OLSKCache').OLSKCacheReadFile(require('path').basename(__dirname), require('path').join(__dirname, '__cached')) || {},
+
 	// DATA
+
+	_DataFoilOLSKCache: require('OLSKCache'),
 
 	DataFetchURLs() {
 		return process.env.ZDA_VITRINE_FETCH_URLS.split(',');
 	},
 
+	_DataContentString (inputData) {
+		return uGet(inputData);
+	},
+
+	// LIFECYCLE
+
+	LifecycleModuleDidLoad () {
+		const _this = this;
+		return mod.DataFetchURLs().map(function (e) {
+			return _this._DataFoilOLSKCache.OLSKCacheResultFetchRenew({
+				ParamMap: _this._ValueCache,
+				ParamKey: e,
+				ParamCallback: (function () {
+					return _this._DataContentString(e);
+				}),
+				ParamInterval: 1000 * 60 * 60 * 24,
+				_ParamCallbackDidFinish: (function () {
+					_this._DataFoilOLSKCache.OLSKCacheWriteFile(_this._ValueCache, require('path').basename(__dirname), require('path').join(__dirname, '__cached'));
+				}),
+			});
+		})
+	},
+
 };
+
+mod.LifecycleModuleDidLoad();
 
 Object.assign(exports, mod);
