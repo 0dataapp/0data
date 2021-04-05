07169edf4bf518da5ec10345b819c790a3c20758
Rosano Coutinho <pub@xeari.com>
2021-04-05 13:57:14 -0300

    Tidy

--- os-app/open-glance/controller.js
+++ os-app/open-glance/controller.js
@@ -25,8 +25,16 @@ const mod = {
 			OLSKRouteMethod: 'get',
 			OLSKRouteSignature: 'ZDAGlanceRoute',
 			OLSKRouteFunction: (function ZDAGlanceRoute (req, res, next) {
-				return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'ui-view'), {
-					ZDAGlanceListData: res.locals.OLSK_SPEC_UI() ? [] : mod.DataProjects(),
+				return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'ui-view'), res.locals.OLSK_SPEC_UI() ? Object.assign({
+					ZDAGlanceListData: [{}],
+				}, Object.fromEntries(Array.from((new URLSearchParams(req.query)).entries()).map(function (e) {
+					if (e[0] === 'ZDAGlanceListData') {
+						e[1] = JSON.parse(e[1]);
+					}
+
+					return e;
+				}))) : {
+					ZDAGlanceListData: mod.DataProjects(),
 				});
 			}),
 			OLSKRouteLanguageCodes: ['en'],
