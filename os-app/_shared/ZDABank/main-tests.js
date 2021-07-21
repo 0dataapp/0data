5a1be09375391509084c83819217cf97eb51876d
Rosano Coutinho <pub@xeari.com>
2021-07-21 11:43:52 -0300

    Spec ZDABank

---
+++ os-app/_shared/ZDABank/main-tests.js
@@ -0,0 +1,81 @@
+const { throws, rejects, deepEqual } = require('assert');
+
+const mod = require('./main.js');
+
+describe('ZDABankURLs', function test_ZDABankURLs() {
+
+	it('returns array', function () {
+		deepEqual(mod.ZDABankURLs(), process.env.ZDA_TASK_BANKS_URLS.split(','));
+	});
+
+});
+
+describe('ZDABankURLRemoteStorage', function test_ZDABankURLRemoteStorage() {
+
+	it('returns string', function () {
+		deepEqual(mod.ZDABankURLRemoteStorage(), mod.ZDABankURLs().filter(function (e) {
+			return e.match(/RemoteStorage/i);
+		}).shift());
+	});
+
+});
+
+describe('ZDABankURLFission', function test_ZDABankURLFission() {
+
+	it('returns string', function () {
+		deepEqual(mod.ZDABankURLFission(), mod.ZDABankURLs().filter(function (e) {
+			return e.match(/Fission/i);
+		}).shift());
+	});
+
+});
+
+describe('ZDABankURLAwesome', function test_ZDABankURLAwesome() {
+
+	it('returns string', function () {
+		deepEqual(mod.ZDABankURLAwesome(), mod.ZDABankURLs().filter(function (e) {
+			return e.match(/Awesome/i);
+		}).shift());
+	});
+
+});
+
+describe('ZDABankURLUnhosted', function test_ZDABankURLUnhosted() {
+
+	it('returns string', function () {
+		deepEqual(mod.ZDABankURLUnhosted(), mod.ZDABankURLs().filter(function (e) {
+			return e.match(/Unhosted/i);
+		}).shift());
+	});
+
+});
+
+describe('ZDABankURLSolid', function test_ZDABankURLSolid() {
+
+	it('returns string', function () {
+		deepEqual(mod.ZDABankURLSolid(), mod.ZDABankURLs().filter(function (e) {
+			return e.match(/Solid/i);
+		}).shift());
+	});
+
+});
+
+describe('ZDABankProtocolProperties', function test_ZDABankProtocolProperties() {
+
+	it('returns object', function () {
+		deepEqual(mod.ZDABankProtocolProperties(), {
+			ZDABankRemoteStorage: {
+				ZDAProtocolName: 'RemoteStorage',
+			},
+			ZDABankFission: {
+				ZDAProtocolName: 'Fission',
+			},
+			ZDABankAwesome: {},
+			ZDABankUnhosted: {},
+			ZDABankSolid: {
+				ZDAProtocolName: 'Solid',
+			},
+		});
+	});
+
+});
---
+++ os-app/_shared/ZDABank/main.js
@@ -0,0 +1,65 @@
+const mod = {
+
+	ZDABankURLs() {
+		return process.env.ZDA_TASK_BANKS_URLS.split(',');
+	},
+
+	ZDABankURLRemoteStorage () {
+		return mod.ZDABankURLs().filter(function (e) {
+			return e.match(/RemoteStorage/i);
+		}).shift();
+	},
+
+	ZDABankURLFission () {
+		return mod.ZDABankURLs().filter(function (e) {
+			return e.match(/Fission/i);
+		}).shift();
+	},
+
+	ZDABankURLAwesome () {
+		return mod.ZDABankURLs().filter(function (e) {
+			return e.match(/Awesome/i);
+		}).shift();
+	},
+
+	ZDABankURLUnhosted () {
+		return mod.ZDABankURLs().filter(function (e) {
+			return e.match(/Unhosted/i);
+		}).shift();
+	},
+
+	ZDABankURLSolid () {
+		return mod.ZDABankURLs().filter(function (e) {
+			return e.match(/Solid/i);
+		}).shift();
+	},
+
+	ZDABankProtocolProperties () {
+		return mod.ZDABankURLs().reduce(function (coll, item) {
+			return Object.assign(coll, {
+				[{
+					[mod.ZDABankURLRemoteStorage()]: 'ZDABankRemoteStorage',
+					[mod.ZDABankURLFission()]: 'ZDABankFission',
+					[mod.ZDABankURLAwesome()]: 'ZDABankAwesome',
+					[mod.ZDABankURLUnhosted()]: 'ZDABankUnhosted',
+					[mod.ZDABankURLSolid()]: 'ZDABankSolid',
+				}[item]]: {
+					[mod.ZDABankURLRemoteStorage()]: {
+						ZDAProtocolName: 'RemoteStorage',
+					},
+					[mod.ZDABankURLFission()]: {
+						ZDAProtocolName: 'Fission',
+					},
+					[mod.ZDABankURLAwesome()]: {},
+					[mod.ZDABankURLUnhosted()]: {},
+					[mod.ZDABankURLSolid()]: {
+						ZDAProtocolName: 'Solid',
+					},
+				}[item],
+			});
+		}, {});
+	},
+
+};
+
+Object.assign(exports, mod);
