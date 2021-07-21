eb9b362bb82a172eabff4aaa5f3f06a8272af5fb
Rosano Coutinho <pub@xeari.com>
2021-07-21 12:57:58 -0300

    Fix typo

--- os-app/open-vitrine/ui-test-_misc.js
+++ os-app/open-vitrine/ui-test-_misc.js
@@ -142,7 +142,7 @@ describe('ZDAVitrine_Misc', function () {
 	
 	});
 
-	require('../task-a-banks/controller.js').DataBankURLs().forEach(function (e, i) {
+	require('../_shared/ZDABank/main.js').ZDABankURLs().forEach(function (e, i) {
 
 		describe('ZDAVitrineProjectsSourcesList', function test_ZDAVitrineProjectsSourcesList () {
 			
