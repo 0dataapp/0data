01002f0dc3104e4b0c5e99ab2b676ae5f8d136c5
Rosano Coutinho <pub@xeari.com>
2021-08-30 11:08:05 -0400

    Rename loc.tests where not necessary

--- os-app/tech-robots/ui-test-_misc.js
+++ os-app/tech-robots/ui-test-_misc.js
@@ -3,7 +3,7 @@ const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 describe('ZDARobots_Misc', function () {
 
 	it('sends text', async function () {
-		browser.assert.deepEqual(await (await browser.fetch('http://loc.tests' + kDefaultRoute.OLSKRoutePath)).text(), require('OLSKRobots').OLSKRobotsTXT([
+		browser.assert.deepEqual(await (await browser.fetch('http://localhost' + kDefaultRoute.OLSKRoutePath)).text(), require('OLSKRobots').OLSKRobotsTXT([
 				require('../open-vitrine/controller.js').OLSKControllerRoutes().shift(),
 				require('../open-glance/controller.js').OLSKControllerRoutes().shift(),
 				require('../api-projects/controller.js').OLSKControllerRoutes().shift(),
