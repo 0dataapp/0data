c043407f1281721177c58cbc7aefdc93e58a2199
Rosano Coutinho <pub@xeari.com>
2022-01-12 16:21:21 -0500

    Purge ROCOEphemerataLink

--- os-app/open-vitrine/ui-test-_access.js
+++ os-app/open-vitrine/ui-test-_access.js
@@ -153,10 +153,6 @@ describe('ZDAVitrine_Access', function () {
 		browser.assert.elements('.ROCOForum', 1);
 	});
 
-	it('shows ROCOEphemerataLink', function () {
-		browser.assert.elements('.ROCOEphemerataLink', 1);
-	});
-
 	it('shows SWARLink', function() {
 		browser.assert.elements('.SWARLink', 1);
 	});
