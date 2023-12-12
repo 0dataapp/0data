7473a2b8fcad1e9467143939fcbbc0eeae42b636
Rosano Coutinho <pub@xeari.com>
2023-12-12 09:17:17 -0300

    Pass failing test

--- os-app/tech-robots/ui-test-_misc.js
+++ os-app/tech-robots/ui-test-_misc.js
@@ -3,7 +3,7 @@ const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 describe('ZDARobots_Misc', function () {
 
 	it('sends text', async function () {
-		return browser.assert.deepEqual(await (await browser.fetch('http://localhost' + kDefaultRoute.OLSKRoutePath)).text(), require('OLSKRobots').OLSKRobotsTXT([
+		return browser.assert.deepEqual(await (await browser.fetch('http://localhost:' + process.env.PORT + kDefaultRoute.OLSKRoutePath)).text(), require('OLSKRobots').OLSKRobotsTXT([
 			require('../open-vitrine/controller.js').OLSKControllerRoutes().shift(),
 			require('../open-glance/controller.js').OLSKControllerRoutes().shift(),
 			require('../api-projects/controller.js').OLSKControllerRoutes().shift(),
